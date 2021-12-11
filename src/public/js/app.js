2370995c402596e57b430cd685c8486b805d91e0
egoing <33287431+a06729@users.noreply.github.com>
2021-12-12 02:50:40 +0900

    유저 접속시 반응하는 css 작업
    아바타 카드 css 및 전송된 파일 없을시 표시하는 css 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,29 +8,48 @@ const myPeer=new Peer(undefined,{
     ]},
 });
 
-let connection = null;
+// let connection = null;
+
+//peerId는 peer server의 고유값 입니다.
 let peerId;
+
+//peer들이 서로 연결 됬을때 쓰기위한 변수
+//myPeer.connect(peerId)함수에서 연결이되면
+//conn.send() 함수로 연결된 peer에 값을 보낼수 있다.
 let conn;
+
+//fireBase db의 고유 글 번호
 let fireDocId;
+
 let file;
 let arrayToStoreChunks = [];
 
-
+//청크 사이즈
 const chunkLength = 65535;
 
-const file_elm=document.querySelector("#fileUpload")
+//파일 태그를 id값으로 접근합니다.
+const file_elm=document.querySelector("#fileUpload");
 
+//guest_avatar_div 클래스 명을 가진 태그에 접근합니다.
 const guest_avatar_div=document.querySelector(".guest_avatar_div");
 
+const none_guest_div=document.querySelector(".none_guest_div");
+
+//아바타에 들어가는 이미지 api 주소 값 입니다.
 const avatar_api_url="https://avatars.dicebear.com/api/bottts";
 
 myPeer.on('open',(id)=>{
     const avatar_img=document.getElementById("my_avatar_img");
+    const avatar_avatar_peerId=document.querySelector(".my_avatar_div__profile__title");
     socket.emit("join-room",ROOM_ID,id,addFireDocId);
     peerId=id;
     avatar_img.src=`${avatar_api_url}/${peerId}.svg`;
     avatar_img.title=peerId;
+    avatar_avatar_peerId.innerHTML=`식별값:${peerId}`;
+
+    guest_avatar_div.style.display="none";
 });
+
 myPeer.on('connection',function(dataConnection){
     dataConnection.on("open",function(){
         dataConnection.on('data',function(data){
@@ -42,7 +61,6 @@ myPeer.on('connection',function(dataConnection){
                 arrayToStoreChunks = []; // resetting array
             }
         });
-        // conn.send("conn 센드");
     });
 });
 
@@ -52,7 +70,6 @@ socket.on("user-connected",(userlist)=>{
     console.log(`peerA:${userlist}`);
     socket.emit("user-connected",ROOM_ID,userlist,addGuestAvatar);
     addGuestAvatar(userlist);
-    // conn = myPeer.connect(userId);
 });
 
 //peerB(브레이브 브라우저)
@@ -60,7 +77,6 @@ socket.on("connected-user",(userlist)=>{
     console.log(`peerB:${userlist}`);
     socket.emit("connected-user",ROOM_ID,userlist);
     addGuestAvatar(userlist);
-    // conn = myPeer.connect(userId);
 });
 
 
@@ -70,35 +86,80 @@ socket.on("user-leave",(userList)=>{
 
 //게스트 아바타를 생성해주는 함수
 function addGuestAvatar(userList){
-    const guest_avatar_all=document.querySelectorAll(".guest_avatar_img");
+
+    //기존에 있는 게스트 img태그 전부 가져오기
+    const guest_avatar_div__profile_all=document.querySelectorAll(".guest_avatar_div__profile");
+    
     //접속할때 마다 게스트 아바타를 전부 리셋
-    guest_avatar_all.forEach((guest)=>{
+    guest_avatar_div__profile_all.forEach((guest)=>{
         guest.remove();
     });
+
+    //접속되어 있는 유저들을 이미지 태그에 표시하는 기능
     userList.forEach(obj => {
         console.log(obj);
         const img_el=document.createElement("img");
+        const guest_avatar_profile_el=document.createElement("div");
+        const guest_avatar_title_el=document.createElement("div");
+        const guest_avatar_fileicon_el=document.createElement("span");
+
         if(obj['peerId']!=peerId){
+            guest_avatar_profile_el.className="guest_avatar_div__profile";
+            
+            guest_avatar_title_el.className="guest_avatar_div__profile__title";
+            guest_avatar_title_el.innerText=`식별값:${obj['peerId']}`;
+            
+            guest_avatar_fileicon_el.className="material-icons guest_avatar_div__profile__file-icon";
+            guest_avatar_fileicon_el.innerText="upload_file";
+            guest_avatar_fileicon_el.title=obj['peerId'];
+
             img_el.src=`${avatar_api_url}/${obj['peerId']}.svg `;
-            img_el.className="guest_avatar_img";
+            img_el.className="guest_avatar_div__profile__img";
             img_el.title=obj['peerId'];
             img_el.id=obj['id'];
-            guest_avatar_div.appendChild(img_el);
+
+            guest_avatar_profile_el.appendChild(img_el);
+            guest_avatar_profile_el.appendChild(guest_avatar_title_el);
+            guest_avatar_profile_el.appendChild(guest_avatar_fileicon_el);
+
+            guest_avatar_div.appendChild(guest_avatar_profile_el);
+
         }
         if(obj['peerId']===peerId){
             fireDocId=obj['id'];
         }
     });
-    document.querySelectorAll(".guest_avatar_img").forEach((elem,index)=>{
+    
+    //접속유저가 없으면 유저창을 안보이게 그리고 유저가 없는 창을 보이게 한다.
+    if(guest_avatar_div.childElementCount===0){
+        guest_avatar_div.style.display="none";
+        none_guest_div.style.display="";
+    }else{ //접속유저가 있을 경우 유저가 없는 창을 안보이게 한다.
+        guest_avatar_div.style.display="";
+        none_guest_div.style.display="none";
+    }
+    //접속유저 이미지 태그를 전부 가져옵니다.
+    document.querySelectorAll(".guest_avatar_div__profile__img").forEach((elem,index)=>{
+        //각 접속유저 이미지 태그에 click시 실행되는 함수를 추가합니다.
+        elem.addEventListener("click",click_guest_avatar);
+    });
+    document.querySelectorAll(".guest_avatar_div__profile__file-icon").forEach((elem,index)=>{
+        //각 접속유저 이미지 태그에 click시 실행되는 함수를 추가합니다.
         elem.addEventListener("click",click_guest_avatar);
     });
 }
 
+
+//소켓 서버에 접속시 fireBase db에서 글에 고유값을 가져옵니다.
+//이것이 있어야 fireBase db에 있는 room에 접속된 유저를 식별하고
+//브라우저 종료시 삭제가 가능합니다.
 function addFireDocId(userData){
     fireDocId=userData['id'];
 }
 
+//브라우저 종료시 실행되는 함수
 function winClose(){
+    //브라우저가 종료되면 서버에 user-leave 함수를 실행
     socket.emit('user-leave',ROOM_ID,peerId,fireDocId);
 }
 
@@ -107,11 +168,11 @@ function click_guest_avatar(event){
     const guest_peer_id=event.target.title;
     conn=myPeer.connect(guest_peer_id);
     
+    //파일태그 클릭을 방생시키는 함수
     document.getElementById('fileUpload').click();
     
     console.log(guest_peer_id);
 
-    
 }
 
 function onReadAsDataURL(event, text) {
@@ -135,14 +196,21 @@ function onReadAsDataURL(event, text) {
     }, 500)
 }
 
+//파일 세이브시 ul태그에 li태그로 추가해서 파일 다운로드 만들기
 function saveToDisk(fileUrl, fileName) {
     const ul = document.getElementById("fileUl");
     const li = document.createElement("li");
+    const none_file_div_el=document.querySelector(".none_file_div");
+    
+    if(none_file_div_el.childElementCount>0){
+        none_file_div_el.style.display="none";
+    }
+    
     var save = document.createElement('a');
     save.href = fileUrl;
     save.target = '_blank';
     save.download = fileName || fileUrl;
-
+    
     save.innerText=fileName;
 
     li.append(save);
@@ -151,7 +219,7 @@ function saveToDisk(fileUrl, fileName) {
 }
 
 
-
+//파일 태그에서 파일이 업로드가되면 발생하는 이벤트
 file_elm.addEventListener("change",(event)=>{
     event.preventDefault();
     
