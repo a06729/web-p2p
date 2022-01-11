a16335798166f2aead7610c97bee2e90e22fc343
egoing <33287431+a06729@users.noreply.github.com>
2022-01-11 20:20:27 +0900

    다크모드 버튼 수정 및 로고 추가
    다크모드 버튼 추가 및 로고 추가

--- src/public/js/darkMode.js
+++ src/public/js/darkMode.js
@@ -8,6 +8,16 @@ function darkMode(){
         localStorage.setItem("theme","light");
     }
 }
+function handleDarkMode(event){
+    if(event.checked==true){
+        document.documentElement.classList.add('dark');
+        localStorage.setItem("theme","dark");
+    }else{
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem("theme","light");
+    }
+
+}
 
 function darkModeInit(){
     if(localStorage.getItem("theme")===null){
@@ -15,9 +25,13 @@ function darkModeInit(){
     }else{
         const teme_mode=localStorage.getItem("theme");
         if(teme_mode==="light"){
+            const darkMode_input=document.getElementById("checkbox");
+            darkMode_input.checked=false;
             document.documentElement.classList.remove('dark');
             localStorage.setItem("theme","light");
         }else{
+            const darkMode_input=document.getElementById("checkbox");
+            darkMode_input.checked=true;
             document.documentElement.classList.add('dark');
             localStorage.setItem("theme","dark");
         }
