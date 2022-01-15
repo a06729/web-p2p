6eaa08defb0ada4308da04530f774d2382462162
egoing <33287431+a06729@users.noreply.github.com>
2022-01-15 19:46:35 +0900

    fix

--- src/fbase/firebase.js
+++ src/fbase/firebase.js
@@ -1,9 +1,24 @@
 const admin = require("firebase-admin");
+require('dotenv').config();
 
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
 
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// const config = {
+//   "projectId": process.env.FIREBASE_PROJECT_ID,
+//   "privateKey": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//   "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+// }
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential:admin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "privateKey": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL:"https://p2pFbase.firebaseio.com"
 });
 const dbService=admin.firestore();
 export default dbService;
